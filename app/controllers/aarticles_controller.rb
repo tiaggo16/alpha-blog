9b5edbe2a6fd770eac2462fa9d684b0e8357269a
Tiago Tabosa <tiaggo95@hotmail.com>
2020-06-28 19:45:19 -0300

    add links and DRY code

--- app/controllers/aarticles_controller.rb
+++ app/controllers/aarticles_controller.rb
@@ -1,7 +1,7 @@
 class AarticlesController < ApplicationController
+  before_action :set_aarticle, only: [:show, :edit, :update, :destroy]
 
   def show
-    @aarticle = Aarticle.find(params[:id])
   end
 
   def index
@@ -13,14 +13,40 @@ class AarticlesController < ApplicationController
   end
 
   def create
-    @aarticle = Aarticle.new(params.require(:aarticle).permit(:title, :description))
+    @aarticle = Aarticle.new(aarticle_params)
     if @aarticle.save
       flash[:notice] = "Article was created successfully."
       redirect_to @aarticle      
     else
       render 'new'
     end
-
   end
 
+  def edit  
+  end
+
+  def update
+    if @aarticle.update(aarticle_params)
+      flash[:notice] = "Article was updated successfullly."
+      redirect_to @aarticle
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @aarticle.destroy
+    redirect_to aarticles_path
+  end
+
+  private
+
+  def set_aarticle
+    @aarticle = Aarticle.find(params[:id])
+  end
+
+  def aarticle_params
+    params.require(:aarticle).permit(:title, :description)
+  end
+ 
 end
