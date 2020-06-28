2b65bf4c571da90e6d425c58ddcb8a4c1572211c
Tiago Tabosa <tiaggo95@hotmail.com>
2020-06-28 17:24:32 -0300

    add article creation

--- app/controllers/aarticles_controller.rb
+++ app/controllers/aarticles_controller.rb
@@ -8,4 +8,14 @@ class AarticlesController < ApplicationController
     @aarticles = Aarticle.all
   end
   
+  def new
+
+  end
+
+  def create
+    @aarticle = Aarticle.new(params.require(:aarticle).permit(:title, :description))
+    @aarticle.save
+    redirect_to @aarticle
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :aarticles, only: [:show, :index]
+  resources :aarticles, only: [:show, :index, :new, :create]
 end
